5669c51263f862a8ebbdc8ed99ba6087b1917bfe
Jose Mtz <sotero.valdez@gmail.com>
2019-10-07 21:55:47 -0700

    insert works!

--- db.go
+++ db.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 
 	_ "github.com/lib/pq"
@@ -16,6 +17,7 @@ const (
 )
 
 var psqlInfo string
+var dbConn *sql.DB
 
 func initDb() {
 	psqlInfo = fmt.Sprintf("host=%s port=%d user=%s "+
@@ -27,6 +29,8 @@ func initDb() {
 		panic(err)
 	}
 
+	dbConn = db
+
 	defer db.Close()
 
 	err = db.Ping()
@@ -35,4 +39,39 @@ func initDb() {
 	}
 
 	fmt.Println("Successfully connected!")
+
+	newArticle := Article{
+		Title:   "Test Article Insert",
+		Desc:    "A new description",
+		Content: "My brand new and longer content column, yay!",
+	}
+
+	addedID, err := insertArticle(&newArticle)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully inserted! Id:", addedID)
+}
+
+func insertArticle(article *Article) (int, error) {
+	if dbConn == nil {
+		return -1, errors.New("DB is nil")
+	}
+
+	if article == nil {
+		return -1, errors.New("Article is nil")
+	}
+	sqlStatement := `
+		INSERT INTO article (title, description, content)
+		VALUES ($1, $2, $3)
+		RETURNING id`
+	id := 0
+	err := dbConn.QueryRow(sqlStatement, article.Title, article.Desc, article.Content).Scan(&id)
+	if err != nil {
+		return -1, err
+	}
+
+	fmt.Println("New record ID is:", id)
+	return id, nil
 }
